92852a8865c4cb00e397639e6259bab48da578d2
labuser <labuser@imac.local>
2018-11-15 19:38:36 -0600

    Initial Commit

---
+++ Assasins UI Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assasins UI Layout
+//
+//  Created by labuser on 11/15/18.
+//  Copyright © 2018 CSE438. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
